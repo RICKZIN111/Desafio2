efc1f493b314b9d314da4f4c9ef5984ac785bf6f
RICKZIN111 <josehenriquesoaress813@gmail.com>
2025-02-13 20:02:38 -0300

    Aula functions adicionada

---
+++ 9.functions.js
@@ -0,0 +1,23 @@
+/* 
+    Functions -> Funções 
+        Trecho de código que é executado
+        SOMENTE quando solicitado
+
+*/
+
+const variavel = "abacatinho"
+
+console.log(variavel)
+
+
+function nomeNaTela() {
+
+    console.log("Henrique")
+}
+
+nomeNaTela() // Pode ser chamada varias vezes
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
+nomeNaTela()
